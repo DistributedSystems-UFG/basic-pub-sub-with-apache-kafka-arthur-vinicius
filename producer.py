761158e7ed7b515c13ded3ee6d89807475b62c1a
arthurdelarge <arthurcandrade@hotmail.com>
2022-12-04 18:10:07 -0300

    various topics

--- producer.py
+++ producer.py
@@ -1,17 +1,21 @@
 from kafka import KafkaProducer
 from const import *
 import sys
+import time
+import random
 
 try:
-    topic = sys.argv[1]
+    topics = sys.argv[1].split(',')
 except:
     print ('Usage: python3 producer <topic_name>')
     exit(1)
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
+    for topic in topics:
+        msg = 'Message id number ' + str(random.randint(1,1000)) + ' for topic ' + topic
+        print ('Sending message: ' + msg)
+        producer.send(topic, value=msg.encode())
+    time.sleep(1)
 
-producer.flush()
+    producer.flush()
